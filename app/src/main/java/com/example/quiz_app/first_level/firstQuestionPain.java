b7a1f76034852f886c25a681098c769b52a3de41
Isuru_Maldeniya <riniranjaya@gmail.com>
2020-04-01 10:32:41 +0530

    strings were updated to level 1

---
+++ app/src/main/java/com/example/quiz_app/first_level/firstQuestionPain.java
@@ -0,0 +1,16 @@
+package com.example.quiz_app.first_level;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quiz_app.R;
+
+public class firstQuestionPain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_question_pain);
+    }
+}
--- app/src/main/java/com/example/quiz_app/first_level/frontDash.java
+++ app/src/main/java/com/example/quiz_app/first_level/frontDash.java
@@ -2,7 +2,9 @@ package com.example.quiz_app.first_level;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.quiz_app.R;
 
@@ -12,5 +14,16 @@ public class frontDash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_dash);
+    }
+
+    public void goInClick(View view) {
+        Intent intent = new Intent(this, firstQuestionPain.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void exitClick(View view) {
+        finish();
+        System.exit(0);
     }
 }
